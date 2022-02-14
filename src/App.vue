1340ced2d48eead41216398cb6ec7dff437c0c11
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-14 15:54:01 +0100

    Added Main component + movies

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
 <template>
-  <div></div>
+  <Main :movies="movies" @click="setQuery" />
 </template>
 
 <script>
+import axios from "axios";
+
+import Main from "./components/Main.vue";
+
 export default {
   name: "App",
-  components: {},
+  data() {
+    return {
+      movies: [],
+      user_key: "1ed3a172e57823689018496d86094f75",
+      query: "lord of the ring",
+      // userQuery: "",
+    };
+  },
+  components: {
+    Main,
+  },
+  methods: {
+    getData() {
+      const config = {
+        params: {
+          api_key: this.user_key,
+          query: this.query,
+          language: "it-IT",
+        },
+      };
+
+      axios
+        .get(`https://api.themoviedb.org/3/search/movie`, config)
+        .then((res) => {
+          this.movies = res.data.results;
+        });
+    },
+    setQuery(val) {
+      this.query = val;
+    },
+  },
+  // computed: {
+  //   updateQuery() {
+  //     return this.query = this.userQuery;
+  //   },
+  // },
+  created() {
+    this.getData();
+  },
 };
 </script>
 
---
+++ src/components/Main.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <div
+      id="search"
+      class="d-flex justify-content-center align-items-center my-5"
+    >
+      <label for="search" class="me-2">Inserisci un film</label>
+      <input v-model="query" id="search" type="text" />
+      <button class="btn btn-danger ms-2" @click="$emit('click', query)">
+        CERCA
+      </button>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div v-for="movie in movies" :key="movie.id" class="col-3 text-center">
+          <ul class="list-unstyled">
+            <li>{{ movie.original_title }}</li>
+            <li>{{ movie.title }}</li>
+            <li>{{ movie.original_language }}</li>
+            <li>{{ movie.vote_average }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: ["movies"],
+  data() {
+    return {
+      query: "",
+    };
+  },
+};
+</script>
+
+<style></style>
